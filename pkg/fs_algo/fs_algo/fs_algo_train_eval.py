715a24b317141b7fa4c102e2ff470d7db8d6e276
Soroush Sorourian <ssorourian@lynker.com>
2025-02-13 16:08:16 -0600

    delete unused mapie boolean arg

--- pkg/fs_algo/fs_algo/fs_algo_train_eval.py
+++ pkg/fs_algo/fs_algo/fs_algo_train_eval.py
@@ -872,7 +872,6 @@ class AlgoTrainEval:
                  metr: str, test_size: float = 0.3,rs: int = 32,
                  test_ids = None,test_id_col:str = 'comid',
                  verbose: bool = False,
-                 mapie: bool = False,
                  mapie_alpha : float = 95,
                  bagging_ci_params: dict = None):
         """The algorithm training and evaluation class.
@@ -903,8 +902,6 @@ class AlgoTrainEval:
         :type test_id_col: str
         :param verbose: Should print, defaults to False.
         :type verbose: bool, optional
-        :param mapie: Calculate MAPIE, defaults to False.
-        :type verbose: bool, optional
         :param mapie_alpha: Confidence interval(s) for MAPIE, defaults to 95.
         :type test_size: float, optional
         :param bagging_ci: Configuration dictionary for Bagging-based uncertainty estimation. 
@@ -922,7 +919,6 @@ class AlgoTrainEval:
         self.rs = rs
         self.dataset_id = dataset_id
         self.verbose = verbose
-        self.mapie = mapie
         self.mapie_alpha = mapie_alpha
         self.bagging_ci_params = bagging_ci_params
 
