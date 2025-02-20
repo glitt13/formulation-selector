fix the forestci with grid search while keeping the forestci calculation under train_eval()

pkg/fs_algo/fs_algo/fs_algo_train_eval.py
@@ -1206,25 +1206,13 @@ class AlgoTrainEval:
             grid_rf = GridSearchCV(pipe_rf, param_grid_rf, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
             
             grid_rf.fit(self.X_train, self.y_train)
-            best_rf = grid_rf.best_estimator_.named_steps['randomforestregressor']
-
-            # # calculate rf confidence intervals from the best rf estimator
-            # ci = self.calculate_rf_uncertainty(grid_rf.best_estimator_.named_steps['randomforestregressor'],
-            #                                     self.X_train, self.X_test)
-
-            # Initialize Uncertainty dictionary
-            uncertainty_dict = {}
-            
-            # Calculate RF confidence intervals using forestci if enabled
-            if self.forestci:
-                uncertainty_dict['forestci'] = self.calculate_forestci_uncertainty(best_rf, self.X_train, self.X_test)
 
             self.algs_dict['rf'] = {'algo': grid_rf.best_estimator_.named_steps['randomforestregressor'],
                                     'pipeline': grid_rf.best_estimator_,
                                     'gridsearchcv': grid_rf,
                                     'type': 'random forest regressor',
                                     'metric': self.metric,
-                                    'Uncertainty': uncertainty_dict
+                                    'Uncertainty': {}
                                     }
         
         if 'mlp' in self.algo_config_grid:  # MULTI-LAYER PERCEPTRON
@@ -1372,10 +1360,15 @@ class AlgoTrainEval:
             self.train_algos()
 
         # Calculate forestci uncertainty if enabled
-        if 'rf' in self.algo_config and self.forestci:
-            self.algs_dict['rf']['Uncertainty']['forestci'] = self.calculate_forestci_uncertainty(
-                self.algs_dict['rf']['algo'], np.array(self.X_train), np.array(self.X_test)
-            )
+        # Determine the best Random Forest model
+        if self.grid_search_algs and 'gridsearchcv' in self.algs_dict['rf']:
+            best_rf_algo = self.algs_dict['rf']['gridsearchcv'].best_estimator_.named_steps['randomforestregressor']
+        else:
+            best_rf_algo = self.algs_dict['rf']['algo']
+        # Compute forestci uncertainty with the best RF model
+        self.algs_dict['rf']['Uncertainty']['forestci'] = self.calculate_forestci_uncertainty(
+            best_rf_algo, np.array(self.X_train), np.array(self.X_test)
+        )
 
         # Calculate Bagging uncertainty if enabled
         for algo_str in self.algo_config.keys():  