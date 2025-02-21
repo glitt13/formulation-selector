13e31d3675b96353ec172fb94996299ed5125bf8
glitt13 <guy.litt@noaa.gov>
2025-02-21 13:29:28 -0700

    feat: add R^2 to regression plots

--- pkg/proc.attr.hydfab/R/proc_attr_grabber.R
+++ pkg/proc.attr.hydfab/R/proc_attr_grabber.R
@@ -1651,12 +1651,6 @@ retr_comids <- function(gage_ids,featureSource,featureID,dir_db_attrs,
   #'  e.g. featureID="USGS-{gage_id}". When defining featureID, it's expected
   #'  that the term 'gage_id' is used as a variable in glue syntax to create featureID
   #'  Refer to ?dataRetrieval::get_nldi_sources() for options to use with nldi_feature
-  #' @param dir_db_attrs Attribute directory path, where the standardized
-  #' comid-gage_id will be stored as a .csv
-  #' @param path_save_gpkg The filepath where the geopackage containing
-  #' comid-gageid-geometry mappings are saved. Default NULL, but strongly recommended
-  #' to use!
-  #' @note 2025-03-07 This needs a deeper refactoring
   #' @export
   #'
   # Changelog/Contributions
