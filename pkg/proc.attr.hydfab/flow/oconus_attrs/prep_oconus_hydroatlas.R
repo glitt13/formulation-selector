659072036b4fea161cb1d1a0954f1488d8ab02ca
glitt13 <guy.litt@noaa.gov>
2025-02-24 18:26:42 -0700

    feat: add parquet file write for all oconus hydroatlas attributes corresponding to the hydrofabric

--- pkg/proc.attr.hydfab/flow/oconus_attrs/prep_oconus_hydroatlas.R
+++ pkg/proc.attr.hydfab/flow/oconus_attrs/prep_oconus_hydroatlas.R
@@ -41,7 +41,6 @@ redo_intersection_analysis <- FALSE # Should we re-run hydroatlas/hydrofabric in
 # Directory containing hydrotlas basins manually downloaded from https://www.hydrosheds.org/products/hydrobasins
 dir_base_hydatl <- "~/noaa/data/hydroatlas/"
 path_haa_global <-  file.path(dir_base_hydatl,"BasinATLAS_Data_v10.gdb","BasinATLAS_v10.gdb")
-
 #------ END USER INPUT HERE
 if(!base::dir.exists(dir_save_base)){
   base::dir.create(dir_save_base,recursive=TRUE)
