b9a8de5ca18f82c6726b8c70de85f9e768a70f9a
glitt13 <guy.litt@noaa.gov>
2025-03-25 19:52:31 -0600

    doc: describe all RaFTS processing steps

--- scripts/eval_ingest/xssa_us/xssaus_proc_rafts_all.sh
+++ scripts/eval_ingest/xssa_us/xssaus_proc_rafts_all.sh
@@ -14,26 +14,36 @@ DIR_R="${DIR_REPO}/pkg/proc.attr.hydfab/flow/"
 
 echo "Running processing from $DIR_CONFIG"
 
-# Prepare the initial dataset here (only needs to happen once)
+# 1. Prepare the initial dataset here with a custom python script (only needs to happen once)
 # "${DIR_CONFIG}prep_xssaus_metrics.py" "${DIR_CONFIG}xssaus_prep_config.yaml"
 
+# The following steps do not require user-input (beyond defining the config files)
 # Print a message to indicate the script is running
 echo "Starting execution of xssa_us process sensitivity scripts..."
 
-# Run the R script to grab attributes
+# 2. Run the R script to grab attributes
 echo "Grabbing attributes"
 Rscript "${DIR_R}fs_attrs_grab.R" "${DIR_CONFIG}xssaus_attr_config.yaml"
 echo "Attribute grabbing completed!"
 
-# Run attribute transformer
+# 2.5 Run attribute transformer
 echo "Generating transformed attributes"
 python3 "${DIR_PY}fs_tfrm_attrs.py" "${DIR_CONFIG}xssaus_attrs_tform.yaml"
 echo "Attribute transformations completed!"
 
-# Train the algorithms
+# 3. Train the algorithms
 echo "Training & testing algorithms..."
 python3 "${DIR_PY}fs_proc_algo_viz.py" "${DIR_CONFIG}xssaus_attr_config.yaml"
 echo "Algorithm training completed!"
 
 # Print a message to indicate all scripts have finished executing
 echo "Attribute grabbing, transformation, and algorithm training executed successfully!"
+
+# 4.1 Identify which locations will be used for prediction, and generate the attributes
+# TODO insert custom script call here
+
+# 4.2 Perform transformations on prediction locations
+# TODO insert modified script call here
+
+# 4.3 Perform the prediction
+# TODO insert the prediction script here
