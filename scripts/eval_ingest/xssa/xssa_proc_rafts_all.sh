2d1b8901f33d1c1434aafde09770214ed53a51b4
glitt13 <guy.litt@noaa.gov>
2025-04-17 14:19:12 -0600

    doc: update steps for running entire processing suite

--- scripts/eval_ingest/xssa/xssa_proc_rafts_all.sh
+++ scripts/eval_ingest/xssa/xssa_proc_rafts_all.sh
@@ -27,29 +27,17 @@ echo "Starting execution of xssa process sensitivity scripts..."
 # 2. Run the R script to grab attributes
 echo "Grabbing attributes"
 Rscript "${DIR_R}fs_attrs_grab.R" "${DIR_CONFIG}xssa_attr_config.yaml"
-if [ $? -ne 0 ]; then
-  echo "Attribute grabbing failed. Attempting next step."
-else
-  echo  "Attribute grabbing completed!"
-fi
+echo "Attribute grabbing completed!"
 
 # 2.5 Run attribute transformer
 echo "Generating transformed attributes"
 python3 "${DIR_PY}fs_tfrm_attrs.py" "${DIR_CONFIG}xssa_attrs_tform.yaml"
-if [ $? -ne 0 ]; then
-  echo "Attribute transformation failed. Attempting next step."
-else
-  echo  "Attribute transformations completed!"
-fi
+echo "Attribute transformations completed!"
 
 # 3. Train the algorithms
 echo "Training & testing algorithms..."
-python3 "${DIR_PY}fs_proc_algo_viz.py" "${DIR_CONFIG}xssa_algo_config.yaml"
-if [ $? -ne 0 ]; then
-  echo "Algorithm training failed. Attempting next step."
-else
-  echo  "Algorithm training completed!"
-fi
+python3 "${DIR_PY}fs_proc_algo_viz.py" "${DIR_CONFIG}xssa_attr_config.yaml"
+echo "Algorithm training completed!"
 
 # Print a message to indicate all scripts have finished executing
 echo "Attribute grabbing, transformation, and algorithm training executed successfully!"
@@ -58,27 +46,14 @@ echo "Attribute grabbing, transformation, and algorithm training executed succes
 echo "Retrieve prediction location attribute data and geometry data"
 # CAUTION: The following Rscript has some custom dependencies in identifying which locations need predicting. Refer to script for details.
 Rscript "${DIR_PRED}gen_pred_locs_xssa.R" "${DIR_CONFIG}xssa_pred_config.yaml"
-if [ $? -ne 0 ]; then
-  echo "Prediction location & attribute acquisition failed. Attempting next step."
-else
-  echo  "Acquired prediction location attribute data and geometry data"
-fi
+echo "Acquired prediction location attribute data and geometry data"
 
 # 4.2 Perform transformations on prediction locations
 echo "Transforming prediction location attribute data"
 python3 "${DIR_PY}fs_tfrm_attrs.py"   "${DIR_CONFIG}xssa_attrs_tform.yaml"
-if [ $? -ne 0 ]; then
-  echo "Prediction attribute transformation failed. Attempting next step."
-else
-  echo  "Transformed prediction location attribute data"
-fi
+echo "Transformed prediction location attribute data"
 
 # 4.3 Perform the prediction & plotting
-echo "Performing predictions"
-python3 "${DIR_PY}fs_pred_algo.py" "${DIR_CONFIG}xssa_pred_config.yaml"
-if [ $? -ne 0 ]; then
-  echo "Prediction & results plotting step failed."
-else
-    echo "xSSA evaluation metrics predictions completed!"
-fi
-echo "RaFTS COMPLETED PROCESSING of xSSA evaluation metrics"
+echo "Performing process predictions"
+python3 "${DIR_PY}fs_pred_algo_viz.py" "${DIR_CONFIG}xssa_pred_config.yaml"
+
