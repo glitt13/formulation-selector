c8691e7d833de0087817a8678bc2847fba5fd41d
Soroush Sorourian <ssorourian@lynker.com>
2025-04-24 15:01:03 -0600

    change fs_proc ro fs_pred in pkg/fs_prep/setup.py

--- pkg/fs_prep/setup.py
+++ pkg/fs_prep/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 # Instructions on package data files: https://sinoroc.gitlab.io/kb/python/package_data.html
 # Instructions for build & install in terminal:
-# > cd /path/to/fs_proc/
+# > cd /path/to/fs_prep/
 # > python setup.py sdist bdist_wheel
 # > pip install -e ./
 setup(
     include_package_data=True,
     package_data={'' : ['./data/*.yaml']},
-    name="fs_proc",
+    name="fs_prep",
     version="0.1.4",
     author="Guy Litt, Ben Choat, Lauren Bolotin",
     author_email="guy.litt@noaa.gov",
