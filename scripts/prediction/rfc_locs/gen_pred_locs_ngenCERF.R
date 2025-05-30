2edb2959bbfae0a45d32b3d60618e4dd3bfff284
glitt13 <guy.litt@noaa.gov>
2025-05-30 07:43:49 -0600

    feat: create prediction location generators for running benchmarking oconus testing and the headwatersbasins from ngen CERF

---
+++ scripts/prediction/rfc_locs/gen_pred_locs_ngenCERF.R
@@ -0,0 +1,146 @@
+#' @title Retrieve attributes for Headwater Basin gaged locations under
+#' consideration for ngen-CERF
+#' @author Guy Litt
+#' @description Given the gageids of ngenCERF forecast locations, grab NHDPlus
+#' catchment attributes to use for prediction (particularly the xSSA process sensitivities)
+#' @details Sections of code embedded inside if(FALSE){} were created to
+#' munge through the patchy nature of NLDI connection limits. If intended to
+#' run continuously, set `pause_nldi_conns` to TRUE and let it run for about 9
+#' hours or so. Once the data have been acquired, pause_nldi_conns may be set to
+#' FALSE and full generation of the prediction metadata parquet file will take
+#' tens of minutes instead (because some locations will never be found)
+#' @example \dontrun{Rscript gen_pred_locs_rfcs_bm25test.R
+#' "{home_dir}/git/formulation-selector/scripts/workflow_configs/legacy/xssa_us/xssaus_pred_config.yaml"
+#' "{home_dir}/noaa/regionalization/data/analyses/basin_selection/HeadwaterBasinGages_ngenCERF_PI3_2024-12-09Calibratable.csv"}
+
+# Changelog/contributions
+#. 2025-05-21 adapted from gen_pred_locs_rfcs.R for bm25 oconus testing
+
+library(nhdplusTools)
+library(proc.attr.hydfab)
+library(dplyr)
+library(glue)
+library(tidyr)
+library(yaml)
+library(future)
+library(future.apply)
+
+
+main <- function(){
+  args <- commandArgs(trailingOnly = TRUE)
+  # Check if the input argument is provided
+  if (length(args) < 2) {
+    stop("Input prediction configuration filepath and full path to nws_nwm_crosswalk.txt must be specified.")
+  }
+  pause_nldi_conns <- FALSE
+# Should NLDI connections be paused?
+
+  # Define args supplied to command line
+  home_dir <- Sys.getenv("HOME")
+  path_cfig_pred <- glue::glue(as.character(args[1])) # path_cfig_pred <- glue::glue("{home_dir}/git/formulation-selector/scripts/workflow_configs/legacy/xssa_us/xssaus_pred_config.yaml")
+  path_file_read <- glue::glue(as.character(args[2])) #path_file_read <- "~/noaa/regionalization/data/analyses/basin_selection/HeadwaterBasinGages_ngenCERF_PI3_2024-12-09Calibratable.csv"
+  # Read in config file
+  if(!base::file.exists(path_cfig_pred)){
+    stop(glue::glue("The provided path_cfig_pred does not exist: {path_cfig_pred}"))
+  }
+  cfig_pred <- yaml::read_yaml(path_cfig_pred)
+  ds_type <- base::unlist(cfig_pred)[['ds_type']]
+  write_type <- base::unlist(cfig_pred[['write_type']])
+   # READ IN ATTRIBUTE CONFIG FILE
+
+  name_attr_config <- cfig_pred[['name_attr_config']]
+  path_attr_config <- proc.attr.hydfab::build_cfig_path(path_cfig_pred,name_attr_config)
+
+  # ------------------------ ATTRIBUTE CONFIGURATION --------------------------- #
+  Retr_Params <- proc.attr.hydfab::attr_cfig_parse(path_attr_config)
+  message(glue::glue("Parsed the attribute config file {path_attr_config}"))
+
+  datasets <- Retr_Params$datasets
+  dir_std_base <- Retr_Params$paths$dir_std_base
+  # ds <- datasets[1]
+  for(ds in datasets){
+    message(glue::glue("Processing dataset {ds}"))
+    # Populate path_meta using the pre-defined ds, dir_std_base, ds_type, write_type
+    path_meta <- glue::glue(Retr_Params$paths$path_meta) # The filepath of the file that generates the list of comids used for prediction
+    if(is.null(path_meta)){
+      stop("Could not populate path_meta. Investigate glue formatting.")
+    }
+
+    ###################### DATASET-SPECIFIC CUSTOM MUNGING #########################
+    # path_file_read <- "~/noaa/regionalization/data/analyses/basin_selection/HeadwaterBasinGages_ngenCERF_PI3_2024-12-09Calibratable.csv"
+    df <- read.csv(path_file_read)
+    col_comid <- "Gage.ID"
+    featureSource <- "nwissite"
+    featureID <- "USGS-{gage_id}"
+    if(TRUE){ # temporarily subset to just CONUS locations
+      df <- df[df$Domain == "CONUS",]
+    }
+
+    ############################ END CUSTOM MUNGING ##############################
+
+    message(glue::glue("Processing {nrow(df)} locations"))
+    # ---------------------- Grab all needed attributes ---------------------- #
+    # --- Create the path to the geopackage:
+    # Define the standardized path to the geopackage based on the input dataset
+    path_save_gpkg <- proc.attr.hydfab::std_path_retr_gpkg_wrap(
+      dir_std_base = Retr_Params$paths$dir_std_base,ds = ds)
+
+    tot_increment <- 50
+    seq_nums <- base::c(base::seq(from=1,nrow(df),tot_increment),nrow(df))[-1]
+    seq_num_bgn <- 1
+    ls_dt_site_feat <- list()
+    ctr <- 0
+    for(seq_num in seq_nums){
+      print(" ----------------------------------------------------- ")
+      print(glue::glue("Acquiring {seq_num} of {nrow(df)}"))
+      ctr <- ctr+1
+      sub_df <- df[seq_num_bgn:seq_num,]
+      seq_num_bgn <- seq_num+1
+      # The unique comids for each location
+      gage_ids <- base::unique(sub_df[[col_comid]])
+      # Now acquire the attributes:
+      sub_dt_site_feat <- try(proc.attr.hydfab::proc_attr_gageids(gage_ids=gage_ids,
+                                                          featureSource=featureSource,
+                                                          featureID=featureID,
+                                                          Retr_Params=Retr_Params,
+                                                          path_save_gpkg = path_save_gpkg,
+                                                          lyrs='network',
+                                                          overwrite=FALSE))
+      if(!"try-error" %in% class(sub_dt_site_feat)){
+        ls_dt_site_feat[[ctr]] <- sub_dt_site_feat
+      } else { # Reduce increments to gageids 1 by 1 in lieu of large chunks:
+        ls_subsub <- list()
+        for(gage_id in gage_ids){
+          subsub_dt_site_feat <- try(proc.attr.hydfab::proc_attr_gageids(gage_ids=gage_id,
+                                                                      featureSource=featureSource,
+                                                                      featureID=featureID,
+                                                                      Retr_Params=Retr_Params,
+                                                                      path_save_gpkg = path_save_gpkg,
+                                                                      lyrs='network',
+                                                                      overwrite=FALSE))
+          if(!"try-error" %in% class(subsub_dt_site_feat)){
+            ls_subsub[[gage_id]] <- subsub_dt_site_feat
+          } else {
+            print(glue::glue("skipping gage_id : {gage_id}"))
+          }
+          ls_dt_site_feat[[ctr]] <- data.table::rbindlist(ls_subsub,fill=TRUE)
+        }
+      }
+      if(pause_nldi_conns){#base::any(base::grepl("usgs_vars",base::names(Retr_Params$vars)))){
+        # will be hitting NLDI and need to limit that
+        max_inc_per_hr <- 380  # max 400 NLDI queries per hour
+        frac_inc_per_hr <- tot_increment/max_inc_per_hr
+        Sys.sleep(60*61*frac_inc_per_hr)
+      }
+    }
+    # -------------generate the prediction.parquet mapper
+    dt_site_feat <- data.table::rbindlist(ls_dt_site_feat)
+    proc.attr.hydfab::write_meta_nldi_feat(dt_site_feat=dt_site_feat,
+                                           path_meta = path_meta)
+
+
+
+  }
+}
+
+main()
---
+++ scripts/prediction/rfc_locs/gen_pred_locs_rfcs_bm25test.R
@@ -0,0 +1,207 @@
+#' @title Retrieve attributes for RFC locations (as available!)
+#'
+#' @author Guy Litt
+#' @description Given the comids of RFC forecast locations, grab NHDPlus
+#' catchment attributes
+#' @details Sections of code embedded inside if(FALSE){} were created to
+#' munge through the patchy nature of NLDI connection limits. If intended to
+#' run continuously, set `pause_nldi_conns` to TRUE and let it run for about 9
+#' hours or so. Once the data have been acquired, pause_nldi_conns may be set to
+#' FALSE and full generation of the prediction metadata parquet file will take
+#' tens of minutes instead (because some locations will never be found)
+#' @example \dontrun{Rscript gen_pred_locs_rfcs_bm25test.R
+#' "{home_dir}/git/formulation-selector/scripts/workflow_configs/legacy/bm_test25/bm25_pred_config.yaml"
+#' "{home_dir}/git/formulation-selector/scripts/prediction/rfc_locs/nws_nwm_crosswalk.txt"}
+
+# Changelog/contributions
+#. 2025-05-21 adapted from gen_pred_locs_rfcs.R for bm25 oconus testing
+
+library(nhdplusTools)
+library(proc.attr.hydfab)
+library(dplyr)
+library(glue)
+library(tidyr)
+library(yaml)
+library(future)
+library(future.apply)
+
+
+main <- function(){
+  args <- commandArgs(trailingOnly = TRUE)
+  # Check if the input argument is provided
+  if (length(args) < 2) {
+    stop("Input prediction configuration filepath and full path to nws_nwm_crosswalk.txt must be specified.")
+  }
+  pause_nldi_conns <- FALSE # Should NLDI connections be paused?
+
+  # Define args supplied to command line
+  home_dir <- Sys.getenv("HOME")
+  path_cfig_pred <- glue::glue(as.character(args[1])) # path_cfig_pred <- glue::glue("{home_dir}/git/formulation-selector/scripts/workflow_configs/legacy/xssa_us/xssaus_pred_config.yaml")
+  path_nwm_crosswalk <- glue::glue(as.character(args[2])) #path_nwm_crosswalk <- "~/git/formulation-selector/scripts/prediction/rfc_locs/nws_nwm_crosswalk.txt"
+  # Read in config file
+  if(!base::file.exists(path_cfig_pred)){
+    stop(glue::glue("The provided path_cfig_pred does not exist: {path_cfig_pred}"))
+  }
+  cfig_pred <- yaml::read_yaml(path_cfig_pred)
+  ds_type <- base::unlist(cfig_pred)[['ds_type']]
+  write_type <- base::unlist(cfig_pred[['write_type']])
+   # READ IN ATTRIBUTE CONFIG FILE
+
+  name_attr_config <- cfig_pred[['name_attr_config']]
+  path_attr_config <- proc.attr.hydfab::build_cfig_path(path_cfig_pred,name_attr_config)
+
+  # ------------------------ ATTRIBUTE CONFIGURATION --------------------------- #
+  Retr_Params <- proc.attr.hydfab::attr_cfig_parse(path_attr_config)
+  message(glue::glue("Parsed the attribute config file {path_attr_config}"))
+
+  datasets <- Retr_Params$datasets
+  dir_std_base <- Retr_Params$paths$dir_std_base
+  # ds <- datasets[1]
+  for(ds in datasets){
+    message(glue::glue("Processing dataset {ds}"))
+    # Populate path_meta using the pre-defined ds, dir_std_base, ds_type, write_type
+    path_meta <- glue::glue(Retr_Params$paths$path_meta) # The filepath of the file that generates the list of comids used for prediction
+    if(is.null(path_meta)){
+      stop("Could not populate path_meta. Investigate glue formatting.")
+    }
+
+    ###################### DATASET-SPECIFIC CUSTOM MUNGING #########################
+    # USER INPUT: Paths to relevant config files
+    # Read file and remove poorly-parsed rows
+    if(base::grepl("nws_nwm_crosswalk.txt",path_nwm_crosswalk)){
+      message("Processing the nws_nwm_crosswalk.txt for prediction")
+      df <- read.delim(file=path_nwm_crosswalk, # Obtained from Gautam Sood at OWP: a file of all RFC station locations
+                       skip=0,sep = "|",col.names = c("nws_station_id","comid"))
+      df <- df[-base::grep("-------+-----", df$nws_station_id),]
+      df_nwm <- df[-which(base::is.na(df$comid)),]
+
+      df_nwm$nws_station_id <- base::gsub(" ","",df_nwm$nws_station_id)
+
+      # Read in the HADS sites
+      df_hads <- proc.attr.hydfab:::read_noaa_hads_sites()
+      df_cmbo_hads <- base::merge(x=df_nwm,y=df_hads,by.x = "nws_station_id", by.y="lid",all.x = TRUE)
+      df_cmbo_miss_h <- df_cmbo_hads[which(is.na(df_cmbo_hads$GOES)),]
+
+      # Read in NWPS:
+      df_nwps <- proc.attr.hydfab::read_noaa_nwps_gauges()
+      df_cmbo_nwps <- base::merge(x=df_nwm,y=df_nwps, by.x = "nws_station_id",by.y="nws_shef_id", all.x=TRUE)
+      df_cmbo_miss_n <- df_cmbo_nwps[which(is.na(df_cmbo_nwps$usgs_id)),]
+
+      # The unknown locations and known locations
+      nws_ids_unkn <- base::intersect(df_cmbo_miss_n$nws_station_id,df_cmbo_miss_h$nws_station_id)
+      nws_ids_have <- base::which(!df_nwm$nws_station_id %in% nws_ids_unkn)
+
+      # TODO identify which locations are oCONUS and consider how to explicitly process using hf_uid
+      oconus_states <- c("AK","HI","PR","VI")
+      base::which(df_cmbo_nwps$state %in% oconus_states)
+      # Next step: refactor proc_attr_gageids to allow different featureSources to be called separately (one for hf_uid, one for comid)
+      #. One idea - allow queries using lat/lon when first query fails (e.g. comid) Would need to make sure to reduce NLDI hits per hour in this case.
+      # TODO allow option to pass in lat/lon to proc_attr_gageids to avoid having to query locations
+
+      if(FALSE){
+        # Retrieve state postal codes (this takes a few mins)
+        hads_postal <- lapply(1:nrow(df_cmbo_hads), function(i)  proc.attr.hydfab::retr_state_terr_postal(
+          lat=df_cmbo_hads$latitude[i],lon=df_cmbo_hads$longitude[i]))
+        df_cmbo_hads$state <- base::unlist(hads_postal)
+        #  Observations: No HI locations recognized after HADS merge: grep("HI", df_cmbo_hads$state)
+      }
+      # --------------------
+      # Simplify - just pick everything with a USGS gage id from NWPS
+      df <- df_cmbo_nwps[!base::is.na(df_cmbo_nwps$usgs_id),]
+      # --------------------
+      col_comid <- 'usgs_id'
+      featureSource <- "nwissite"
+      featureID <- "USGS-{gage_id}"
+
+
+
+      if(FALSE){ # short-term munging, reducing the total df based on known data availability
+        path_save_gpkg <- proc.attr.hydfab::std_path_retr_gpkg_wrap(
+          dir_std_base = Retr_Params$paths$dir_std_base,ds = Retr_Params$datasets[[1]])
+        gpk <- sf::st_read(path_save_gpkg)
+        need_ids <- lapply(df$usgs_id, function(x) base::paste0("USGS-",x)) %>% unlist()
+        reduced_df <- df[which(!need_ids %in% gpk$featureID),]
+        df <- reduced_df
+      }
+
+    } else {
+      stop("NOT PROCESSING - EDIT HERE")
+      df <- read.csv(path_nwm_crosswalk)
+      col_comid <- "hf_id"
+      featureSource <- "comid"
+      featureID <- "{gage_id}"
+    }
+
+    # Reduce the df:
+    if(base::any(base::duplicated(df[[col_comid]]))){
+      df <- df[-base::which(base::duplicated(df[[col_comid]])),]
+    }
+
+    ############################ END CUSTOM MUNGING ##############################
+
+    message(glue::glue("Processing {nrow(df)} locations"))
+    # ---------------------- Grab all needed attributes ---------------------- #
+    # --- Create the path to the geopackage:
+    # Define the standardized path to the geopackage based on the input dataset
+    path_save_gpkg <- proc.attr.hydfab::std_path_retr_gpkg_wrap(
+      dir_std_base = Retr_Params$paths$dir_std_base,ds = ds)
+
+    tot_increment <- 50
+    seq_nums <- base::c(base::seq(from=1,nrow(df),tot_increment),nrow(df))[-1]
+    seq_num_bgn <- 1
+    ls_dt_site_feat <- list()
+    ctr <- 0
+    for(seq_num in seq_nums){
+      print(" ----------------------------------------------------- ")
+      print(glue::glue("Acquiring {seq_num} of {nrow(df)}"))
+      ctr <- ctr+1
+      sub_df <- df[seq_num_bgn:seq_num,]
+      seq_num_bgn <- seq_num+1
+      # The unique comids for each location
+      gage_ids <- base::unique(sub_df[[col_comid]])
+      # Now acquire the attributes:
+      sub_dt_site_feat <- try(proc.attr.hydfab::proc_attr_gageids(gage_ids=gage_ids,
+                                                          featureSource=featureSource,
+                                                          featureID=featureID,
+                                                          Retr_Params=Retr_Params,
+                                                          path_save_gpkg = path_save_gpkg,
+                                                          lyrs='network',
+                                                          overwrite=FALSE))
+      if(!"try-error" %in% class(sub_dt_site_feat)){
+        ls_dt_site_feat[[ctr]] <- sub_dt_site_feat
+      } else { # Reduce increments to gageids 1 by 1 in lieu of large chunks:
+        ls_subsub <- list()
+        for(gage_id in gage_ids){
+          subsub_dt_site_feat <- try(proc.attr.hydfab::proc_attr_gageids(gage_ids=gage_id,
+                                                                      featureSource=featureSource,
+                                                                      featureID=featureID,
+                                                                      Retr_Params=Retr_Params,
+                                                                      path_save_gpkg = path_save_gpkg,
+                                                                      lyrs='network',
+                                                                      overwrite=FALSE))
+          if(!"try-error" %in% class(subsub_dt_site_feat)){
+            ls_subsub[[gage_id]] <- subsub_dt_site_feat
+          } else {
+            print(glue::glue("skipping gage_id : {gage_id}"))
+          }
+          ls_dt_site_feat[[ctr]] <- data.table::rbindlist(ls_subsub,fill=TRUE)
+        }
+      }
+      if(pause_nldi_conns){#base::any(base::grepl("usgs_vars",base::names(Retr_Params$vars)))){
+        # will be hitting NLDI and need to limit that
+        max_inc_per_hr <- 380  # max 400 NLDI queries per hour
+        frac_inc_per_hr <- tot_increment/max_inc_per_hr
+        Sys.sleep(60*61*frac_inc_per_hr)
+      }
+    }
+    # -------------generate the prediction.parquet mapper
+    dt_site_feat <- data.table::rbindlist(ls_dt_site_feat)
+    proc.attr.hydfab::write_meta_nldi_feat(dt_site_feat=dt_site_feat,
+                                           path_meta = path_meta)
+
+
+
+  }
+}
+
+main()
