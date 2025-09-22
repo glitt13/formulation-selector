999ca5f56ba0e564bd81f4d141991db1214e68ed
glitt13 <guy.litt@noaa.gov>
2025-09-22 16:52:30 -0600

    fix: remove hardcoding of prep config file, correct the name_tfrm_config inside pred config

--- scripts/prediction/xssa_sub/gen_pred_locs_xssa.R
+++ scripts/prediction/xssa_sub/gen_pred_locs_xssa.R
@@ -63,8 +63,8 @@ main <- function(){
 
   ###################### DATASET-SPECIFIC CUSTOM MUNGING #########################
   # USER INPUT: Paths to relevant config files
-  path_raw_config <- glue::glue("{home_dir}/git/formulation-selector/scripts/eval_ingest/xssa/xssa_prep_config.yaml")
-
+  name_prep_config <- cfig_pred[['name_prep_config']]
+  path_raw_config <- proc.attr.hydfab::build_cfig_path(path_cfig_pred,name_prep_config)#glue::glue("{home_dir}/git/formulation-selector/scripts/eval_ingest/xssa/xssa_prep_config.yaml")
 
 
   # --------------------------- INPUT DATA READ -------------------------------- #
