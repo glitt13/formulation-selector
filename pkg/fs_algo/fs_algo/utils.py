c8428e0d65ad6382560632ccc7083d6234c1755c
glitt13 <guy.litt@noaa.gov>
2026-08-10 16:29:44 -0600

    fix: add layer-name config spec for path_hf_finl_gpkg

--- pkg/fs_algo/fs_algo/utils.py
+++ pkg/fs_algo/fs_algo/utils.py
@@ -432,6 +432,7 @@ class PredConfigParser:
         path_crosswalk_ids = pred_cfg.get('path_crosswalk_ids',None)
         overwrite_sql = pred_cfg.get('overwrite_sql', False)
         path_hf_finl_gpkg = pred_cfg.get('path_hf_finl_gpkg', None)
+        layr_hf_finl_gpkg = pred_cfg.get('layr_hf_finl_gpkg',None)
         algo_select = pred_cfg.get('algo_select', None)
 
         # Compile dictionary
@@ -459,6 +460,7 @@ class PredConfigParser:
             'pred_gpkg_id_col':pred_gpkg_id_col,
             'path_crosswalk_ids':path_crosswalk_ids,
             'path_hf_finl_gpkg': path_hf_finl_gpkg,
+            'layr_hf_finl_gpkg':layr_hf_finl_gpkg,
             'overwrite_sql': overwrite_sql,
             'algo_select': algo_select,
         }   
