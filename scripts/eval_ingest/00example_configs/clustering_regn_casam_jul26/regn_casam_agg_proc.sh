ad7b1caa26291a2b23e102b9b3f0c238c3770385
glitt13 <guy.litt@noaa.gov>
2026-08-17 08:24:47 -0600

    doc: provide example xgb/adaboost & clustering configurations

---
+++ scripts/eval_ingest/00example_configs/clustering_regn_casam_jul26/regn_casam_agg_proc.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# RaFTS processing the regionalization testing dataset with hfATLAS data
+# Instructions:
+# Make this script executable using: chmod +x regn_casam_agg_proc.sh
+# Run by calling in terminal: ./regn_casam_agg_proc.sh
+
+# -----------------------------------------------------------------------------
+# ERROR HANDLING
+# -----------------------------------------------------------------------------
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error and exit immediately.
+# -o pipefail: Ensure errors in piped commands are caught.
+set -euo pipefail || { echo "..."; exit 1; }
+
+# -----------------------------------------------------------------------------
+# PATH DEFINITIONS
+# -----------------------------------------------------------------------------
+echo "Using system home directory as basis for all paths: $HOME"
+DIR_REPO="$HOME/git/formulation-selector"
+DIR_CONFIG="${DIR_REPO}/scripts/eval_ingest/regn_hf4vpu1_clst_jul26_test2"
+#DIR_PRED="${DIR_REPO}/scripts/prediction/rfc_locs"
+DIR_PREP="${DIR_REPO}/pkg/fs_prep/fs_prep/flow"
+DIR_PY="${DIR_REPO}/pkg/fs_algo/fs_algo/flow"
+
+echo "Running processing from $DIR_CONFIG"
+
+# -----------------------------------------------------------------------------
+# EXECUTION WORKFLOW
+# -----------------------------------------------------------------------------
+
+# 1. Prepare the initial dataset
+echo "Starting execution of hfATLAS parameter regionalization scripts..."
+echo "--> Preparing the initial dataset..."
+uv run --project "${DIR_REPO}/pkg" python "${DIR_CONFIG}/prep_regn_test_agg.py" "${DIR_CONFIG}/regn_casam_agg_prep_config.yaml" || {
+    echo "ERROR: Dataset preparation failed. Exiting."
+    exit 1
+}
+
+# 2. Run the hfATLAS standardized prep script
+echo "--> Grabbing attributes..."
+uv run --project "${DIR_REPO}/pkg" python "${DIR_PREP}/fs_agg_hfatl_basin.py" \
+    --path_prep_config "${DIR_CONFIG}/regn_casam_agg_prep_config.yaml" \
+    --path_attr_config "${DIR_CONFIG}/regn_casam_attr_config.yaml" || {
+    echo "ERROR: Attribute grabbing failed. Exiting."
+    exit 1
+}
+echo "Attribute grabbing completed successfully!"
+
+# 3. Train the algorithms 
+echo "--> Training & testing algorithms..."
+uv run --project "${DIR_REPO}/pkg" python "${DIR_PY}/fs_proc_algo_pool.py" "${DIR_CONFIG}/regn_casam_algo_config.yaml" --chunk_size 4 || {
+    echo "ERROR: Algorithm training failed. Exiting."
+    exit 1
+}
+echo "Algorithm training completed successfully!"
+
+# 4. Perform the prediction
+echo "--> Performing cluster predictions..."
+uv run --project "${DIR_REPO}/pkg" python "${DIR_PY}/fs_pred_algo.py" "${DIR_CONFIG}/regn_casam_pred_config_hf4.yaml" || {
+    echo "ERROR: cluster predictions failed. Exiting."
+    exit 1
+}
+echo "Cluster predictions completed successfully!"
+
+# 5. Pair the donor-receivers
+echo "--> Performing donor-receiver pairing..."
+uv run --project "${DIR_REPO}/pkg" python "${DIR_PY}/fs_pair_donors.py" "${DIR_CONFIG}/regn_casam_pred_config_hf4.yaml" || {
+    echo "ERROR: Donor-receiver pairings failed. Exiting."
+    exit 1
+}
+echo "Donor-receiver pairings completed successfully!"
+
+# 6. Map the predictions
+echo "--> Plotting the cluster predictions on static map..."
+uv run --project "${DIR_REPO}/pkg" python "${DIR_PY}/fs_map_pred_hfatl.py" "${DIR_CONFIG}/regn_casam_pred_config_hf4.yaml" || {
+    echo "ERROR: Prediction mapping failed. Exiting."
+    exit 1
+}
+echo "Completed prediction mapping!"
+
+# 7. Write Parameters to Compiled GeoPackage
+echo "--> Compiling regionalized parameters into master GPKG..."
+uv run --project "${DIR_REPO}/pkg" python "${DIR_PY}/fs_regn_params_gpkg.py" "${DIR_CONFIG}/regn_casam_pred_config_hf4.yaml" || {
+    echo "ERROR: GPKG compilation failed for ${MODEL}. Exiting."
+    exit 1
+}
+
+
+
+echo "========================================================================"
+echo "SUCCESS: Finished the hfATLAS regionalization predictions!"
+echo "========================================================================"
